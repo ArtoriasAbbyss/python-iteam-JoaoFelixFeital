519f13b33ab08aba6c61952dfc4d0ef36c806a7e
ArtoriasAbbyss <joao.feitalfelix@outlook.com>
2026-05-21 12:52:20 -0400

    Update solucao.py

--- desafios/desafio-01/solucao.py
+++ desafios/desafio-01/solucao.py
@@ -1,5 +1,9 @@
 # Desafio 01 — Seu Primeiro Script
-# Aluno: (seu nome aqui)
-# Data:  (data de entrega)
+# Aluno: joao felix feital
+# Data:  21/05/2026
 
 # ── Escreva sua solução abaixo ──────────────────────────────────────────────
+nome = input("Digite seu nome: ")
+anoNascimento = int(input("Digite seu ano de nascimento: "))
+anoAtual = int(2026)
+print("Seu nome é: ", nome, "e sua idade é ",anoAtual - anoNascimento, " anos.")
