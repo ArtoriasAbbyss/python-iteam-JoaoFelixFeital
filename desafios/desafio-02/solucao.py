f6486785d0c581b7b60e5e0c7c1ecc349da0bead
ArtoriasAbbyss <joao.feitalfelix@outlook.com>
2026-05-22 15:19:23 -0400

    Update solucao.py

--- desafios/desafio-02/solucao.py
+++ desafios/desafio-02/solucao.py
@@ -3,3 +3,7 @@
 # Data:  (data de entrega)
 
 # ── Escreva sua solução abaixo ──────────────────────────────────────────────
+nome = str(input("Digite seu nome:"))
+altura = float(input("Digite sua altura:"))
+massa = float(input("Digite sua massa corporal: "))
+print(f"{nome}, seu imc é {massa / (altura * altura)}")
